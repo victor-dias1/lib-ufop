de1ff4c583ebb8971d333d2a26a4c876ddac8b15
Dias-Victor <68074434+Dias-Victor@users.noreply.github.com>
2020-08-20 12:48:42 -0300

    css test

--- forms.php
+++ forms.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="style.php" type="text/css">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Bootstrap Dashboard by Bootstrapious.com</title>
