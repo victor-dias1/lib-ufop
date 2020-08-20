10c9913a8b7b9d424e5a0802b53e8c86fdab6e22
Dias-Victor <68074434+Dias-Victor@users.noreply.github.com>
2020-08-20 15:13:24 -0300

    bd test

--- conexao.php
+++ conexao.php
@@ -1,14 +1,16 @@
 <?php
-     $servidor = "ec2-34-236-215-156.compute-1.amazonaws.com";
-     $porta = 5432;
-     $bancoDeDados = "d1t5mmnc4idlmu";
-     $usuario = "axroruviokavnt";
-     $senha = "bcf94125d7fa163aca1bbf577a92baf9921566a8fce44c9bf9156b82e1507500";
+    $servidor = "ec2-34-236-215-156.compute-1.amazonaws.com";
+    $porta = 5432;
+    $bancoDeDados = "d1t5mmnc4idlmu";
+    $usuario = "axroruviokavnt";
+    $senha = "bcf94125d7fa163aca1bbf577a92baf9921566a8fce44c9bf9156b82e1507500";
 
-     $conexao = pg_connect("host=$servidor port=$porta dbname=$bancoDeDados " +
+    $conexao = pg_connect("host=$servidor port=$porta dbname=$bancoDeDados " +
                                      "user=$usuario password=$senha");
-     if(!$conexao) 
-        {
-            die("Não foi possível se conectar ao banco de dados.");
-        }
+    if(!$conexao) 
+    {
+        die("Não foi possível se conectar ao banco de dados.");
+    }
+    
+    $query = $conexao("INSERT INTO usuarios VALUES (123, 1, .victor., .dias., 123, .exemplo@gmail.com., .1234.)");
 ?>
