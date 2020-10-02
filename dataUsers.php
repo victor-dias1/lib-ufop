b34c89a3a82942abfa88ea25d489479bd6765b52
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-02 10:19:54 -0300

    voltando html

--- dataUsers.php
+++ dataUsers.php
@@ -30,8 +30,14 @@
 	</head>
 	<body>
 		<h1>Usuários</h1>
-		<table id="listar-usuario"
-            class="table table-striped table-bordered dt-responsive datatable"
-            style="border-collapse: collapse; border-spacing: 5; width: 100%;">
+		<table id="listar-livros" class="display" style="width:100%">
+			<thead>
+				<tr>
+					<th>Nome</th>
+					<th>Sobrenome</th>
+					<th>Matrícula</th>
+				</tr>
+			</thead>
+		</table>
 	</body>
 </html>
