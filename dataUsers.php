a283b1f8d39d3dce71c985d43588460b3bc8b229
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-02 08:16:05 -0300

    teste visualização

--- dataUsers.php
+++ dataUsers.php
@@ -89,35 +89,6 @@
         	</div>
       		</div>
 		</nav>
-		<!-- navbar-->
-		<header class="header">
-        <nav class="navbar">
-          <div class="container-fluid">
-            <div class="navbar-holder d-flex align-items-center justify-content-between">
-              <div class="navbar-header">
-                <a id="toggle-btn" href="#" class="menu-btn">
-                  <i class="icon-bars"></i>
-                </a>
-                <a href="main.php" class="navbar-brand">
-                  <div class="brand-text d-none d-md-inline-block">
-                    <span>Bootstrap </span>
-                    <strong class="text-primary">Dashboard</strong>
-                  </div>
-                </a>
-              </div>
-              <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
-                <!-- Log out-->
-                <li class="nav-item">
-                  <a href="logout.php" class="nav-link logout"> 
-                    <span class="d-none d-sm-inline-block">Logout</span>
-                    <i class="fa fa-sign-out"></i>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-		  </header>
 		  <!-- Breadcrumb-->
 		  <div class="breadcrumb-holder">
         <div class="container-fluid">
