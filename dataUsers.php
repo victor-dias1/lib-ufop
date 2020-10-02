5d2de9613d4eadeda6c0eee2e6bd793e0ef7de81
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-02 10:16:20 -0300

    bootstrap4 dataTable

--- dataUsers.php
+++ dataUsers.php
@@ -12,9 +12,9 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Biblioteca</title>
-		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
-		<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+		<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+		<script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
 		<script type="text/javascript" language="javascript">
 		$(document).ready(function() {
 			$('#listar-usuario').DataTable({			
