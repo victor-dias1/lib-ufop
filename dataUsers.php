40b8151c133e920b97f155d9287f29c388ebf27b
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-06 05:33:58 -0300

    col-lg-14

--- dataUsers.php
+++ dataUsers.php
@@ -53,7 +53,7 @@
 		<h1>Usuários</h1>
 		<div class="container">	
 			<div class="row justify-content-center">">
-				<div class="col-lg-10">
+				<div class="col-lg-14">
             		<div class="table-responsive">
 						<table id="listar-usuario" class="display">
             				<thead>
