4837ca7e48d3fa09404ed399c46b91909c449182
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-22 00:29:52 -0300

    testando dataTables youtube

--- dataUsers.php
+++ dataUsers.php
@@ -12,9 +12,15 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Biblioteca</title>
-		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
+		<!--<link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css">
+		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 		<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-		<script type="text/javascript" language="javascript">
+		<script type="text/javascript" language="javascript">-->!
+		<link rel="stylesheet" href="https://getbootstrap.com/dist/css/bootstrap.min.css">
+  		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.10/css/jquery.dataTables.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  		<script type="text/javascript" src="https://cdn.datatables.net/1.10.10/js/jquery.dataTables.min.js"></script>
+		<script>
 		$(document).ready(function() {
 			$('#listar-usuario').DataTable({			
 				"processing": true,
