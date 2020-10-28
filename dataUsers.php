8f90733dbe6900b8fb72d373697642972aca717b
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-28 02:56:30 -0300

    testando o "x" do modal

--- dataUsers.php
+++ dataUsers.php
@@ -94,7 +94,7 @@ if (!isset($_SESSION['cpf'])) {
 					<div class="modal-content">
 						<div class="modal-header">
 							<h5 class="modal-title">Modal title</h5>
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close" href="javascript: fechar();">
 								<span aria-hidden="true">&times;</span>
 							</button>
 						</div>
