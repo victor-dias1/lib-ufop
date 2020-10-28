aa58661060319218aea133dc2946224bf569eff1
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-28 02:50:48 -0300

    testando com input

--- dataUsers.php
+++ dataUsers.php
@@ -102,7 +102,7 @@ if (!isset($_SESSION['cpf'])) {
 							<p>Matrícula</p>
 							<form method="POST" action="processDeleteUsers.php">
 								<p><input type="text" name="matricula">
-								<p><a class="btn-primary" type="submit" role="button" data-dismiss="modal">Excluir</a></p>
+								<p><input type="submit" value="Excluir" class="btn-primary" role="button" data-dismiss="modal">Excluir></p>
 							</form>	
 						</div>
 						<div class="modal-footer">
