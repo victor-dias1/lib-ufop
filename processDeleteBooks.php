66df054cc01c6180b38b2184501186412b2210fb
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-28 03:34:18 -0300

    corrigindo path pós delete de livros

--- processDeleteBooks.php
+++ processDeleteBooks.php
@@ -3,13 +3,13 @@
     include_once("conexao.php");
 
     $matricula = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
-    $result_livro = "DELETE FROM livros WHERE id = '$id'";
+    $result_livro = "DELETE FROM livros WHERE id_livros = '$id'";
     $resultado_livro = pg_query($conexao, $result_livro);
 
     if($resultado_livro){
-        $_SESSION['msg'] = "<p style='color:green;'> Usuário deletado com sucesso!</p>";
-        header("Location: dataUsers.php");
+        $_SESSION['msg'] = "<p style='color:green;'> Livro deletado com sucesso!</p>";
+        header("Location: dataBooks.php");
     }else{
-        $_SESSION['msg'] = "<p style='color:red;'> Erro ao deletar usuário!</p>";
-        header("Location: dataUsers.php");
+        $_SESSION['msg'] = "<p style='color:red;'> Erro ao deletar livro!</p>";
+        header("Location: dataBooks.php");
     }
