388f2ce30e19bfc934c023f1df455172c6a63129
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-28 02:46:15 -0300

    trocando href para submit

--- dataUsers.php
+++ dataUsers.php
@@ -102,7 +102,7 @@ if (!isset($_SESSION['cpf'])) {
 							<p>Matrícula</p>
 							<form method="POST" action="processDeleteUsers.php">
 								<p><input type="text" name="matricula">
-								<a class="btn-primary" href="javascript: fechar();" role="button" data-dismiss="modal">Excluir</a>
+								<p><a class="btn-primary" type="submit" role="button" data-dismiss="modal">Excluir</a></p>
 							</form>	
 						</div>
 						<div class="modal-footer">
