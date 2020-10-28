b8b8f2a3a9424bde7118e6ce77ebe365a778a15e
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-28 03:12:49 -0300

    testando delete de usuários

--- processDeleteUsers.php
+++ processDeleteUsers.php
@@ -3,8 +3,15 @@
     include_once("conexao.php");
 
     $matricula = filter_input(INPUT_POST, 'matricula', FILTER_SANITIZE_NUMBER_INT);
-    //$matricula = $_POST['matricula'];
-    echo $matricula;
-    //$result_usuario = "DELETE FROM usuarios WHERE matricula = '$matricula'";
-    //$resultado_usuario = pg_query($conexao, $result_usuario);
+    $result_usuario = "DELETE FROM usuarios WHERE matricula = '$matricula'";
+    $resultado_usuario = pg_query($conexao, $result_usuario);
+
+    if(pg_affected_rows($conexao)){
+        $_SESSION['msg'] = "<p style='color:green;'> Usuário deletado com sucesso!</p>";
+        header("dataUsers.php");
+    }else{
+        $_SESSION['msg'] = "<p style='color:red;'> Erro ao deletar usuário!</p>";
+        header("dataUsers.php");
+    }
+
 ?>
