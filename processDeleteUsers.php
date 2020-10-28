1046945b892fadd6b8e1650e3b4adafec4bd9f9c
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-10-28 03:08:28 -0300

    testando filtro do post

--- processDeleteUsers.php
+++ processDeleteUsers.php
@@ -2,7 +2,8 @@
     session_start();
     include_once("conexao.php");
 
-    $matricula = $_POST['matricula'];
+    $matricula = filter_input(INPUT_POST, 'matricula', FILTER_SANITIZE_NUMBER_INT);
+    //$matricula = $_POST['matricula'];
     echo $matricula;
     //$result_usuario = "DELETE FROM usuarios WHERE matricula = '$matricula'";
     //$resultado_usuario = pg_query($conexao, $result_usuario);
