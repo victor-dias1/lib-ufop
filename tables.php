4726cea5c311c3b518e3f3e71a8bd5c8ed78f067
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-04 03:12:51 -0300

    testando header como função

--- tables.php
+++ tables.php
@@ -9,12 +9,17 @@
 ?>
 <html>
   <head>
-    <meta charset="utf-8">
+    <?php
+      include_once("header.php");
+    ?>
+    
+    <title>Biblioteca</title>
+    <!--<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="all,follow">
-		<title>Biblioteca</title>
+    <meta name="robots" content="all,follow">-->
+		
 		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 		<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
@@ -30,25 +35,25 @@
 			});
 		} );
 		</script>
-    <!-- Bootstrap CSS-->
+    <!-- Bootstrap CSS->
     <link rel="stylesheet" href="vendor_new/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome CSS-->
+    <!- Font Awesome CSS->
     <link rel="stylesheet" href="vendor_new/font-awesome/css/font-awesome.min.css">
-    <!-- Fontastic Custom icon font-->
+    <!- Fontastic Custom icon font->
     <link rel="stylesheet" href="css/fontastic.css">
-    <!-- Google fonts - Roboto -->
+    <!- Google fonts - Roboto ->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
-    <!-- jQuery Circle-->
+    <!- jQuery Circle->
     <link rel="stylesheet" href="css/grasp_mobile_progress_circle-1.0.0.min.css">
-    <!-- Custom Scrollbar-->
+    <!- Custom Scrollbar->
     <link rel="stylesheet" href="vendor_new/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css">
-    <!-- theme stylesheet-->
+    <!- theme stylesheet->
     <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
-    <!-- Custom stylesheet - for your changes-->
+    <!- Custom stylesheet - for your changes->
     <link rel="stylesheet" href="css/custom.css">
-    <!-- Favicon-->
+    <!- Favicon->
     <link rel="shortcut icon" href="img/favicon.ico">
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+    <-- Tweaks for older IEs--><!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
   </head>
@@ -63,19 +68,19 @@
 				  </tr>
 			    </thead>
 		    </table>-->
-    <!-- Side Navbar -->
+    <!-- Side Navbar 
     <nav class="side-navbar">
       <div class="side-navbar-wrapper">
-        <!-- Sidebar Header    -->
+        <!- Sidebar Header    
         <div class="sidenav-header d-flex align-items-center justify-content-center">
-          <!-- User Info-->
+          <!- User Info
           <div class="sidenav-header-inner text-center"><img src="img/avatar-7.jpeg" alt="person" class="img-fluid rounded-circle">
             <h2 class="h5">Victor Dias</h2><span>Web Developer</span>
           </div>
-          <!-- Small Brand information, appears on minimized sidebar-->
+          <!- Small Brand information, appears on minimized sidebar
           <div class="sidenav-header-logo"><a href="main.php" class="brand-small text-center"> <strong>B</strong><strong class="text-primary">D</strong></a></div>
         </div>
-        <!-- Sidebar Navigation Menus-->
+        <!- Sidebar Navigation Menus
         <div class="main-menu">
           <h5 class="sidenav-heading">Menu</h5>
           <ul id="side-main-menu" class="side-menu list-unstyled">                  
@@ -87,8 +92,8 @@
       </div>
     </nav>
     <div class="page">
-      <!-- navbar-->
-      <header class="header">
+      <!- navbar-->
+      <!--<header class="header">
         <nav class="navbar">
           <div class="container-fluid">
             <div class="navbar-holder d-flex align-items-center justify-content-between">
@@ -104,7 +109,7 @@
                 </a>
               </div>
               <ul class="nav-menu list-unstyled d-flex flex-md-row align-items-md-center">
-                <!-- Log out-->
+                 Log out
                 <li class="nav-item">
                   <a href="logout.php" class="nav-link logout"> 
                     <span class="d-none d-sm-inline-block">Logout</span>
@@ -115,7 +120,7 @@
             </div>
           </div>
         </nav>
-      </header>
+      </header>-->
       <!-- Breadcrumb-->
       <div class="breadcrumb-holder">
         <div class="container-fluid">
