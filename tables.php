695045921c1bde215da2eb19f3aad775fc6113b5
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-11-04 03:19:49 -0300

    corrigindo title

--- tables.php
+++ tables.php
@@ -9,12 +9,12 @@
 ?>
 <html>
   <head> 
-    <title>Biblioteca</title>
-    <!--<meta charset="utf-8"-->
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">-->
+    <title>Biblioteca</title>
 		
 		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 		<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
