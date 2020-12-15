8d13c110f7d103d8fb230913ce9c338343d38a86
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-15 20:20:53 -0300

    novo template

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['cpf'], $_SESSION['pnome'], $_SESSION['email']);
-    
-    $_SESSION['msg'] = "Deslogado com sucesso!";
-    header("Location: login.php");
-?>
