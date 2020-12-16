b1366c9f2c588c1dabbea2bb364d3e7aa1f7b5f2
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-15 23:14:44 -0300

    adicionando o js e o css na raiz

--- gerenciaUsuarios.php
+++ gerenciaUsuarios.php
@@ -9,12 +9,15 @@ $row_usuario_usuario = pg_query($conexao, $result_usuario);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <title>Reservas</title>
+
     <link rel="icon" href="imagem/favicon.ico">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script defer src="js/fontawesome-all.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/fontawesome.min.css">
     <link rel="stylesheet" href="css/dashboard.css">
+
+    <script defer src="js/fontawesome-all.min.js"></script>
 </head>
 
 <body>
@@ -125,9 +128,9 @@ $row_usuario_usuario = pg_query($conexao, $result_usuario);
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/dashboard.js"></script>
 </body>
 
