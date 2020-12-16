b34766876940f2ad132fa2cf3d6eaa3c070b2789
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-15 22:45:26 -0300

    testando dataTables com link

--- gerenciaUsuarios.php
+++ gerenciaUsuarios.php
@@ -1,49 +1,32 @@
 <!DOCTYPE html>
 <?php
-    include_once("conexao.php");
-    $result_usuario = "SELECT * FROM usuarios";
-    $row_usuario_usuario = pg_query($conexao, $result_usuario);
+include_once("conexao.php");
+$result_usuario = "SELECT * FROM usuarios";
+$row_usuario_usuario = pg_query($conexao, $result_usuario);
 ?>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Usuários</title>
+    <title>Reservas</title>
     <link rel="icon" href="imagem/favicon.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script defer src="js/fontawesome-all.min.js"></script>
     <link rel="stylesheet" href="css/fontawesome.min.css">
     <link rel="stylesheet" href="css/dashboard.css">
 
-    <!-- Datatables CSS library -->
-    <link rel="stylesheet" type="text/css" href="css/jquery.dataTables.min.css">
 
-    <!-- Bootstrap Core -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.22/r-2.2.6/datatables.min.css" />
 
-    <!-- Datatables Bootstrap library-->
-    <link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap4.min.css">
-
-    <!-- DataTables Bootstrap 4 integration -->
-    <script src="js/dataTables.bootstrap4.min.js"> </script>
-
-    <!-- JQuery library -->
-    <script src="js/jquery-3.5.1.js"></script>
-
-    <!-- Datatables JS library -->
-    <script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.22/r-2.2.6/datatables.min.js"></script>
 
     <script>
         $(document).ready(function() {
-            $('#listaUsuarios').dataTable({
-                "pagingType": "full_numbers",
-                language: {
-                    url: "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Portuguese-Brasil.json"
-                },
-            });
+            $('#listaUsuarios').DataTable();
         });
     </script>
+
 </head>
 
 <body>
@@ -158,6 +141,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="js/dashboard.js"></script>
-    </body>
+</body>
 
 </html>
