d40229907b2c5b1a36af9c4a048b1902c669a4ad
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-15 23:56:43 -0300

    voltando pra raiz

--- gerenciaUsuarios.php
+++ gerenciaUsuarios.php
@@ -128,9 +128,9 @@ $row_usuario_usuario = pg_query($conexao, $result_usuario);
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
     <script src="js/dashboard.js"></script>
     <script src="js/jquery-3.5.1.min.js"></script>
 </body>
