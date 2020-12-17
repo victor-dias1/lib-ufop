66522c8bf2d91c47ff52eaa1e31505118b5a7188
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-16 23:16:21 -0300

    incluindo jquery slim min

--- gerenciaUsuarios.php
+++ gerenciaUsuarios.php
@@ -26,6 +26,7 @@ $row_usuario_usuario = pg_query($conexao, $result_usuario);
 
     <!-- JQuery -->
     <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="js/jquery.dataTables.min.js"></script>
 
     <!-- JS BootStrap 4 -->
@@ -154,10 +155,6 @@ $row_usuario_usuario = pg_query($conexao, $result_usuario);
         </div>
     </div> -->
 
-    <!-- <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/dashboard.js"></script> -->
-    
 </body>
 
 </html>
