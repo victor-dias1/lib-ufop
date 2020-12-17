b9aeaaf974a1e544b509e6b108945e199cd6183c
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-16 21:42:12 -0300

    menu de testes

---
+++ includes/sidebar.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<div class="d-flex">
+    <nav class="sidebar">
+        <ul class="list-unstyled">
+            <li>
+                <a href="#submenu2" data-toggle="collapse"> Gerência</a>
+                <ul id="submenu2" class="list-unstyled collapse">
+                    <li><a href="gerenciaUsuarios.php"><i class="fas fa-users"></i> Usuários</a></li>
+                    <li><a href="gerenciaLivros.php"><i class="fab fa-book"></i> Livros</a></li>
+                </ul>
+
+            </li>
+            <li><a href="gerenciaEmprestimos.php"> Empréstimos</a></li>
+            <li><a href="gerenciaReservas.php"> Reservas</a></li>
+            <li><a href="#"><i class="fas fa-sign-out-alt"></i> Sair</a></li>
+        </ul>
+    </nav>
+</div>
---
+++ menu_teste.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<HTML lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Celke</title>
+    <link rel="icon" href="imagem/favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script defer src="js/fontawesome-all.min.js"></script>
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/dashboard.css">
+</head>
+
+<body>
+    <?php include_once('includes/header.php');?>
+
+    <?php include_once('includes/sidebar.php');?>
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="js/dashboard.js"></script>
+</body>
+
+</HTML>
