9b8842838c5c6d5e24d1dfc1ee21b4530c672c53
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-16 23:20:37 -0300

    testando popper min js

--- gerenciaUsuarios.php
+++ gerenciaUsuarios.php
@@ -26,7 +26,7 @@ $row_usuario_usuario = pg_query($conexao, $result_usuario);
 
     <!-- JQuery -->
     <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
     <script src="js/jquery.dataTables.min.js"></script>
 
     <!-- JS BootStrap 4 -->
