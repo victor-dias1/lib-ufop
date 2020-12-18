eb9292bfecb0d008579c08dc55f6ac394af7b7e2
ufop-projetobiblioteca <68976234+ufop-projetobiblioteca@users.noreply.github.com>
2020-12-18 18:04:09 -0300

    gerencia livros - modais

--- gerenciaLivros.php
+++ gerenciaLivros.php
@@ -91,6 +91,148 @@ $resultado_livro = pg_query($conexao, $result_livro);
                                         <button type="button" class="btn btn-sm btn-outline-danger">Apagar</button>
                                     </td>
                                 </tr>
+                                <!-- Inicio Modal Visualizar -->
+                                <div class="modal fade" id="myModal<?php echo $row_livros['id_livros']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title text-center" id="myModalLabel">Dados do Livro</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <dl class="row">
+                                                    <dt class="col-sm-3">ID:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_livros['id_livros']; ?></dd>
+
+                                                    <dt class="col-sm-3">Nome:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_livros['nome']; ?></dd>
+
+                                                    <dt class="col-sm-3">Autor:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_livros['autor']; ?></dd>
+
+                                                    <dt class="col-sm-3">Edicao:</dt>
+                                                    <dd class="col-sm-9"><?php echo $row_livros['edicao']; ?></dd>
+                                                </dl>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <a class="btn btn-outline-success" role="button" data-dismiss="modal" data-toggle="modal" data-target="#modalCadastrarExemplar">Cadastrar Exemplar</a>
+                                                <a class="btn btn-outline-info" role="button" data-dismiss="modal" data-toggle="modal" data-target="#modalEditar<?php echo $row_livros['id_livros']; ?>">Editar</a>
+                                                <a class="btn btn-outline-danger" role="button" data-dismiss="modal" aria-label="Close">Cancelar</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Fim Modal Visualizar-->
+
+                                <!-- Inicio Modal Cadastrar Livro-->
+                                <div class="modal fade" id="modalCadastrarLivro" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                    <div class="modal-dialog modal-lg" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title text-center" id="myModalLabel">Cadastrar Livro</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form method="POST" action="processa/novoLivro.php">
+                                                    <div class="form-group row">
+                                                        <label class="col-sm-2 col-form-label">ID:</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" name="id_livro" required maxlength="30">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label class="col-sm-2 col-form-label">Título:</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" name="nome" required maxlength="30">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label class="col-sm-2 col-form-label">ISBN:</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" name="isbn" required maxlength="30">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label class="col-sm-2 col-form-label">Autor:</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" name="autor" required maxlength="30">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label class="col-sm-2 col-form-label">Edição:</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" name="edicao" required maxlength="10">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label class="col-sm-2 col-form-label">Sessão:</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" name="sessao_id" required maxlength="30">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <div class="col-sm-10">
+                                                            <button type="submit" class="btn btn-outline-success">Cadastrar</button>
+                                                            <button class="btn btn-outline-danger" data-dismiss="modal" aria-label="Close">Cancelar</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Fim Modal Cadastrar Livro -->
+
+                                <!-- Inicio Modal Editar-->
+                                <div class="modal fade" id="modalEditar<?php echo $row_livros['id_livros']; ?>" tabindex="-1" role="dialog">
+                                    <div class="modal-dialog modal-lg" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title">Editar livro</h5>
+                                                <button type="button" class="close" data-dismiss="modal">
+                                                    <span>&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form method="POST" action="processa/editaLivro.php">
+                                                    <div class="form-group row">
+                                                        <label class="col-sm-2 col-form-label">Título:</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" name="nome" value="<?php echo $row_livros['nome'] ?>" required maxlength="30">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label class="col-sm-2 col-form-label">ISBN:</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" name="isbn" value="<?php echo $row_livros['isbn'] ?>" required maxlength="30">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label class="col-sm-2 col-form-label">Autor:</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" name="autor" value="<?php echo $row_livros['autor'] ?>" required maxlength="30">
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group row">
+                                                        <label class="col-sm-2 col-form-label">Edição:</label>
+                                                        <div class="col-sm-10">
+                                                            <input type="text" class="form-control" name="edicao" value="<?php echo $row_livros['edicao'] ?>" required maxlength="30">
+                                                        </div>
+                                                    </div>
+                                                    <input type="hidden" name="id_livros" value="<?php echo $row_livros['id_livros']; ?>">
+                                                    <div class="form-group row">
+                                                        <div class="col-sm-10">
+                                                            <button type="submit" class="btn btn-outline-success">Salvar</button>
+                                                            <button class="btn btn-outline-danger" data-dismiss="modal" aria-label="Close">Cancelar</button>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                <!-- Fim Modal Editar-->
                             <?php
                             }
                             ?>
