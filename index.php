c06cb07bf50c951f5d76a29657c286e072b5054b
Dias-Victor <68074434+Dias-Victor@users.noreply.github.com>
2021-05-12 19:49:48 -0300

    testando login celke

--- index.php
+++ index.php
@@ -5,35 +5,31 @@ session_start();
 
 <html lang="pt-br">
 
+
 <head>
-  <meta charset="utf-8">
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
   <title>Biblioteca</title>
-
-  <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
-
-  <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="css/signin.css" rel="stylesheet">
+  <link rel="icon" href="imagem/favicon.ico">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/signin.css">
 </head>
 
 <body class="text-center">
-  <form class="form-signin" action="valida_login.php" method="POST">
-    <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Faça o login</h1>
-    <input type="email" id="inputEmail" class="form-control" placeholder="nome@exemplo.com" required autofocus>
-    <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
-    <!---<div class="checkbox mb-3">
-      <label>
-        <input type="checkbox" value="remember-me"> Lembrar
-      </label>
-    </div>--->
-    <input class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+  <form class="form-signin" action="valida_login.php" method="post">
+    <img class="mb-4" src="imagem/logo.png" alt="Celke" width="72" height="72">
+    <h1 class="h3 mb-3 font-weight-normal">Área Restrita</h1>
+
+    <div class="form-group">
+      <label>E-mail:</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="nome@exemplo.com">
+    </div>
+    <div class="form-group">
+      <label>Senha:</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="******">
+    </div>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Fazer login</button>
+    <p class="text-center">Esqueceu a senha?</p>
   </form>
 </body>
 
