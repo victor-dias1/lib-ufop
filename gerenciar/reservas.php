434befc87e5ee89d8e978438385290318d751692
Dias-Victor <68074434+Dias-Victor@users.noreply.github.com>
2021-05-28 14:01:24 -0300

    deletar reserva

--- gerenciar/reservas.php
+++ gerenciar/reservas.php
@@ -200,6 +200,28 @@ $resultado_reservas = pg_query($conexao, $result_reservas);
                                     </div>
                                 </div>
                                 <!-- Fim Modal Cadastrar Empréstimo-->
+
+                                <!-- Inicio Modal Apagar-->
+                                <div class="modal fade" id="modalApagar<?php echo $row_reservas['rcodigoexemplar']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title text-center" id="myModalLabel">Deletar Reserva</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <blockquote class="blockquote">
+                                                    <p class="mb-0">Tem certeza que deseja excluir a reserva do seu Banco de Dados?</p>
+                                                </blockquote>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <a class="btn btn-outline-danger" href='../processar/deletar/reserva.php?id=<?php echo $row_reservas['rcodigoexemplar']; ?>' role="button">Excluir</a>
+                                                <a class="btn btn-outline-primary" role="button" data-dismiss="modal" aria-label="Close">Cancelar</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Fim Modal Apagar-->
                             <?php
                             }
                             ?>
