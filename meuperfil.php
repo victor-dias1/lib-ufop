95ddfd1a979a4150b37b213e8b9e7a7cce7f8497
Dias-Victor <68074434+Dias-Victor@users.noreply.github.com>
2021-05-28 13:34:35 -0300

    testando meu perfil

--- meuperfil.php
+++ meuperfil.php
@@ -1,53 +1,89 @@
+<!DOCTYPE html>
 <?php
-include_once("../conexao.php");
-$result_usuario = "SELECT * FROM usuarios";
-$row_usuario_usuario = pg_query($conexao, $result_usuario);
+session_start();
+if (!isset($_SESSION['cpf'])) {
+    $_SESSION['login_error'] = "Faça login para continuar!";
+    header("Location: index.php");
+}
 ?>
 
-<!DOCTYPE html>
 <html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>Usuários</title>
-
-    <!-- CSS Template -->
-    <link rel="icon" href="../imagem/favicon.ico">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/fontawesome.min.css">
-    <link rel="stylesheet" href="../css/dashboard.css">
-
-    <!-- JS Template -->
-    <script defer src="../js/fontawesome-all.min.js"></script>
-
-    <!-- CSS dataTables -->
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/dataTables.bootstrap4.min.css">
-
-    <!-- JS and JQuery dataTables -->
-    <script src="../js/jquery-3.5.1.min.js"></script>
-    <script src="../js/jquery.dataTables.min.js"></script>
-    <script src="../js/dataTables.bootstrap4.min.js"></script>
+    <title>Biblioteca</title>
+    <link rel="icon" href="imagem/favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script defer src="js/fontawesome-all.min.js"></script>
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <!-- Begin Inspectlet Asynchronous Code -->
+    <script type="text/javascript">
+        (function() {
+            window.__insp = window.__insp || [];
+            __insp.push(['wid', 266308637]);
+            var ldinsp = function() {
+                if (typeof window.__inspld != "undefined") return;
+                window.__inspld = 1;
+                var insp = document.createElement('script');
+                insp.type = 'text/javascript';
+                insp.async = true;
+                insp.id = "inspsync";
+                insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=266308637&r=' + Math.floor(new Date().getTime() / 3600000);
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(insp, x);
+            };
+            setTimeout(ldinsp, 0);
+        })();
+    </script>
+    <!-- End Inspectlet Asynchronous Code -->
 </head>
 
 <body>
+    <nav class="navbar navbar-expand navbar-dark bg-primary">
+        <a class="sidebar-toggle text-light mr-3">
+            <span class="navbar-toggler-icon"></span>
+        </a>
+        <a class="navbar-brand" href="main.php">Biblioteca</a>
 
-    <?php include_once('../includes/header.php'); ?>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle menu-header" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown">
+                        <img class="rounded-circle" src="imagem/icon.png" width="20" height="20"> &nbsp;<span class="d-none d-sm-inline">Usuário</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="meuperfil.php"><i class="fas fa-user"></i> Meu Perfil</a>
+                        <a class="dropdown-item" href="sair.php"><i class="fas fa-sign-out-alt"></i> Sair</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
-    <main>
-        <?php include_once('../includes/sidebar.php'); ?>
-        <div class="content p-1">
-            <div class="d-flex justify-content-center">
-                <h1 class="display-3">Meu Perfil</h1>
-            </div>
-    </main>
+    <div class="d-flex">
+        <nav class="sidebar">
+            <ul class="list-unstyled">
+                <li>
+                    <a href="#submenu2" data-toggle="collapse"> Gerência</a>
+                    <ul id="submenu2" class="list-unstyled collapse">
+                        <li><a href="gerenciar/usuarios.php"><i class="fas fa-users"></i> Usuários</a></li>
+                        <li><a href="gerenciar/livros.php"><i class="fas fa-book"></i> Livros</a></li>
+                    </ul>
 
-    <!-- JS Template -->
+                </li>
+                <li><a href="gerenciar/emprestimos.php"> Empréstimos</a></li>
+                <li><a href="gerenciar/reservas.php"> Reservas</a></li>
+                <li><a href="sair.php"><i class="fas fa-sign-out-alt"></i> Sair</a></li>
+            </ul>
+        </nav>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="../js/dashboard.js"></script>
+    <script src="js/dashboard.js"></script>
 </body>
 
 </html>
