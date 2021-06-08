7e23e8f62d0ae9e6210b3846c4cecf887c667982
Dias-Victor <68074434+Dias-Victor@users.noreply.github.com>
2021-06-08 01:18:22 -0300

    testando com svg

--- gerenciar/emprestimos.php
+++ gerenciar/emprestimos.php
@@ -106,7 +106,14 @@ $resultado_emprestimos = pg_query($conexao, $result_emprestimos);
                                     <td><?php echo $row_emprestimos['dataemprestimo']; ?></td>
                                     <td><?php echo $row_emprestimos['dataentrega']; ?></td>
                                     <td>
-                                        <button type="button" class="bi bi-plus-circle" data-toggle="modal" data-target="#myModal<?php echo $row_emprestimos['ecodigoexemplar']; ?>"></button>
+                                        <a href="#" class="text-decoration-none">
+                                            Example link text
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                                            </svg>
+                                        </a>
+                                        <button type="button" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#myModal<?php echo $row_emprestimos['ecodigoexemplar']; ?>">Visualizar</button>
                                         <button type="button" class="btn btn-sm btn-outline-danger" data-toggle="modal" data-target="#modalApagar<?php echo $row_emprestimos['ecodigoexemplar']; ?>">Apagar</button>
                                     </td>
                                 </tr>
